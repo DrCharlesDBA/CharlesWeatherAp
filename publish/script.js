1556298d44f2f478cef449f0a529822efac13716
Charles <117086143+DrCharlesDBA@users.noreply.github.com>
2023-10-16 10:11:25 +0200

    v3 weather icon changing

--- publish/script.js
+++ publish/script.js
@@ -1,7 +1,7 @@
 //Here I declred search input and button.
 const searchInput = document.querySelector('.search input');
 const searchBtn = document.querySelector('.search button');
-const searchBtn = document.querySelector('.search button');
+const image = document.querySelector('.icon');
 
 async function getWeather(city) {
     var res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d03e2854a891e07b0aa41324f9780644&units=metric`);
@@ -11,6 +11,24 @@ async function getWeather(city) {
     document.querySelector('.city').innerHTML = data.name;
     document.querySelector('.humidityP').innerHTML = Math.round(data.main.humidity) + " %";
     document.querySelector('.windS').innerHTML = Math.round(data.wind.speed) + "km/h";
+
+    if(data.weather[0].main == "Clouds"){
+        image.src = "./WeatherImages/cloudy.png"
+    } else if(data.weather[0] == "Dew"){
+        image.src = "./WeatherImages/dew.png"
+    } else if(data.weather[0] == "Clear"){
+        image.src = "./WeatherImages/clear-sky.png"
+    } else if(data.weather[0] == "Rain"){
+        image.src = "./WeatherImages/rainkid.png"
+    } else if(data.weather[0] == "Mist"){
+        image.src = "./WeatherImages/mist.png"
+    } else if(data.weather[0] == "Wind"){
+        image.src = "./WeatherImages/wind.png"
+    } else if(data.weather[0] == "Snow"){
+        image.src = "./WeatherImages/snowing.png"
+    }else if(data.weather[0] == "Mist"){
+        image.src = "./WeatherImages/mist.png"
+    }
 }
 //Declaration of the Btn's event listener
 searchBtn.addEventListener('click', () => {
