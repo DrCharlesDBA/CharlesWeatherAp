6a087393d80b37387cb9e4dc39a8f2df1bbccc27
Charles <117086143+DrCharlesDBA@users.noreply.github.com>
2023-10-16 15:07:36 +0200

    v3.1 trying to show various weather icons

--- publish/script.js
+++ publish/script.js
@@ -5,6 +5,11 @@ const image = document.querySelector('.icon');
 
 async function getWeather(city) {
     var res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d03e2854a891e07b0aa41324f9780644&units=metric`);
+    if(res.status == 404) {
+      document.querySelector('.error').style.display = "block";
+    } else {
+        document.querySelector('.error').style.display = "none";
+    }
     var data = await res.json();
     console.log(data);
     document.querySelector('.celcius').innerHTML = Math.round(data.main.temp) + " °C";
@@ -12,21 +17,21 @@ async function getWeather(city) {
     document.querySelector('.humidityP').innerHTML = Math.round(data.main.humidity) + " %";
     document.querySelector('.windS').innerHTML = Math.round(data.wind.speed) + "km/h";
 
-    if(data.weather[0].main == "Clouds"){
-        image.src = "./WeatherImages/cloudy.png"
-    } else if(data.weather[0] == "Dew"){
-        image.src = "./WeatherImages/dew.png"
-    } else if(data.weather[0] == "Clear"){
-        image.src = "./WeatherImages/clear-sky.png"
-    } else if(data.weather[0] == "Rain"){
-        image.src = "./WeatherImages/rainkid.png"
-    } else if(data.weather[0] == "Mist"){
-        image.src = "./WeatherImages/mist.png"
-    } else if(data.weather[0] == "Wind"){
-        image.src = "./WeatherImages/wind.png"
-    } else if(data.weather[0] == "Snow"){
-        image.src = "./WeatherImages/snowing.png"
-    }else if(data.weather[0] == "Mist"){
+    if(data.weather[0].main == "Clouds") {
+        image.src="./WeatherImages/clouds.png"
+    } else if(data.weather[0].main == "Dew") {
+        image.src="./WeatherImages/dew.png"
+    } else if(data.weather[0].main == "Clear") {
+        image.src="./WeatherImages/clear-sky.png"
+    } else if(data.weather[0].main == "Rain") {
+        image.src="./WeatherImages/rainkid.png"
+    } else if(data.weather[0].main == "Mist") {
+        image.src="./WeatherImages/mist.png"
+    } else if(data.weather[0].main == "Wind") {
+        image.src="./WeatherImages/wind.png"
+    } else if(data.weather[0].main == "Snow") {
+        image.src="./WeatherImages/snowing.png"
+    }else if(data.weather[0].main == "Mist") {
         image.src = "./WeatherImages/mist.png"
     }
 }
